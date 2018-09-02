eec690761b9710ddc828449526a10cf35b49d444
Administrator <Administrator@192.168.201.105>
2018-09-02 14:14:18 +0800

    2018090202

--- MyJavEE/src/tw/brad/Brad05.java
+++ MyJavEE/src/tw/brad/Brad05.java
@@ -2,6 +2,7 @@ package tw.brad;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -26,7 +27,15 @@ public class Brad05 extends HttpServlet {
 		response.setContentType("text/html; charset=UTF-8");
 		
 		PrintWriter writer = response.getWriter();
-		writer.append("您好, 全世界");
+		writer.append("您好, 全世界<hr>");
+		
+		Enumeration<String> params = request.getParameterNames();
+		while (params.hasMoreElements()) {
+			String param = params.nextElement();
+			String value = request.getParameter(param);
+			String nvalue = new String(value.getBytes("ISO-8859-1"), "UTF-8");
+			writer.append(param + ":" + value + ":" + nvalue + "<br>");
+		}
 		writer.flush();
 		
 	}
